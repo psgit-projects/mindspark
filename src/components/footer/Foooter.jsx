6e0ad0f6ca8420bada2eb6ede2cbf033ecf6d589
Priti Saxena <180224914+psgit-projects@users.noreply.github.com>
2026-05-25 23:28:31 +0530

    Update footer heading text

--- src/components/footer/Foooter.jsx
+++ src/components/footer/Foooter.jsx
@@ -3,7 +3,7 @@ import './ft.css'
 const Foooter = () => {
   return (
     <div className='ft'>
-      <h3>Stay Touch Mind Spark</h3>
+      <h3>Stay Connected </h3>
       <ul>
         <li>Terms of Service</li>
         <li>Privacy Policy</li>
